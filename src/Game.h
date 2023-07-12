daf0c18051f3f7de98a0e71af8b62bfba85163f1
ThGnommy <th.brandoli@gmail.com>
2023-07-12 23:15:56 +0200

    first commit

---
+++ src/Game.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+class Game {
+  private:
+    bool isRunning;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+  public:
+    Game();
+    ~Game();
+    void Initialize(); 
+    void Run();
+    void ProcessInput();
+    void Update();
+    void Render();
+    void Destroy();
+};
