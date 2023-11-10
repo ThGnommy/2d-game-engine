15798b72eb9c51810f162411346e55a2e531d502
Thomas Brandoli <thomas.brandoli@zuru.com>
2023-11-10 10:26:17 +0100

    render texture

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,50 +1,59 @@
-#include "Game.h"
-#include <SDL2/SDL.h>
+#include <cstddef>
 #include <iostream>
+#include <SDL2/SDL.h>
+#include "Game.h"
+#include "SDL2/SDL_rect.h"
+#include "SDL2/SDL_render.h"
+#include "SDL2/SDL_surface.h"
+#include <SDL2/SDL_image.h>
 
-
-Game::Game() {
+Game::Game()
+{
   isRunning = false;
   std::cout << "Game constructor called!" << std::endl;
 }
 
-Game::~Game() {
+Game::~Game()
+{
   std::cout << "Game destructor called!" << std::endl;
 }
 
-void Game::Initialize() {
-  if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+void Game::Initialize()
+{
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+  {
     std::cerr << "Error initializing SDL." << std::endl;
     return;
   }
 
-  // setup w and h 
+  // setup w and h
   SDL_DisplayMode displayMode;
   SDL_GetCurrentDisplayMode(0, &displayMode);
   windowWidth = displayMode.w;
   windowHeight = displayMode.h;
 
   window = SDL_CreateWindow(
-    NULL,
-    SDL_WINDOWPOS_CENTERED, 
-    SDL_WINDOWPOS_CENTERED, 
-    windowWidth, 
-    windowHeight,
-    SDL_WINDOW_ALWAYS_ON_TOP
-   );
+      NULL,
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      windowWidth,
+      windowHeight,
+      SDL_WINDOW_ALWAYS_ON_TOP);
 
-  if(!window) {
+  if (!window)
+  {
     std::cerr << "Error creating SDL window." << std::endl;
     return;
   }
 
   // Create a 2D rendering context for a window.
   renderer = SDL_CreateRenderer(
-    window,
-    -1,
-    SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+      window,
+      -1,
+      SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-  if(!renderer) {
+  if (!renderer)
+  {
     std::cerr << "Error creating SDL renderer." << std::endl;
     return;
   }
@@ -55,49 +64,75 @@ void Game::Initialize() {
   isRunning = true;
 }
 
-void Game::Run() {
-  while(isRunning) {
+void Game::ProcessInput()
+{
+  SDL_Event sdlEvent;
+
+  // Poll the events
+  while (SDL_PollEvent(&sdlEvent))
+  {
+    switch (sdlEvent.type)
+    {
+    case SDL_QUIT:
+      isRunning = false;
+      break;
+    case SDL_KEYDOWN:
+      if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)
+      {
+        isRunning = false;
+      }
+      break;
+    }
+  }
+}
+
+void Game::Setup()
+{
+  // TODO: Inizialize game objects...
+}
+
+void Game::Update()
+{
+  // TODO: update game objects...
+}
+
+void Game::Render()
+{
+  SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+  SDL_RenderClear(renderer);
+
+  // TODO: Render all game objects...
+
+  // Draw a PNG texture
+
+  SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+  SDL_FreeSurface(surface);
+
+  SDL_Rect dstRect = {200, 200, 32, 32};
+
+  SDL_DestroyTexture(texture);
+  SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+
+  // 'Paint' the window
+  SDL_RenderPresent(renderer);
+}
+
+void Game::Run()
+{
+
+  Setup();
+
+  while (isRunning)
+  {
     ProcessInput();
     Update();
     Render();
   }
 }
 
-void Game::ProcessInput() {
-  SDL_Event sdlEvent;
-
-  // Poll the events
-  while(SDL_PollEvent(&sdlEvent)) {
-    switch(sdlEvent.type) {
-      case SDL_QUIT:
-        isRunning = false;
-        break;
-      case SDL_KEYDOWN:
-        if(sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
-          isRunning = false;
-        }
-        break;
-    }
-  }
-
-}
-
-void Game::Update() {
-  //TODO: update game objects...
-}
-
-void Game::Render() {
-  // Select the color
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-  SDL_RenderClear(renderer);
-
-  // TODO: Render all game objects...
-
-  // 'Paint' the window
-  SDL_RenderPresent(renderer);
-}
-
-void Game::Destroy() {
+void Game::Destroy()
+{
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
